1e573ec72e8beaf81214470de244d72d1c7d56ae
furkandel <57002551+Furkan-del@users.noreply.github.com>
2023-08-14 12:19:47 +0300

    fixedTests

--- src/test/kotlin/com/example/luggagesystemflightapi/service/FlightServiceTest.kt
+++ src/test/kotlin/com/example/luggagesystemflightapi/service/FlightServiceTest.kt
@@ -1,15 +1,19 @@
 package com.example.luggagesystemflightapi.service
 
+import com.example.luggagesystemflightapi.domain.event.FlightCreatedEvent
 import com.example.luggagesystemflightapi.domain.model.Flight
 import com.example.luggagesystemflightapi.producer.FlightCreatedEventProducer
 import com.example.luggagesystemflightapi.repository.FlightRepository
 
 import com.example.luggagesystemflightapi.utils.createFlightRequest
+import com.example.luggagesystemflightapi.utils.flightCreatedEvent
 import com.example.luggagesystemflightapi.utils.getFlight
+import io.mockk.Runs
 import io.mockk.clearMocks
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOf
@@ -53,18 +57,18 @@ class FlightServiceTest {
 
         }
 
-      /*  @Test
-        fun `when flightService#getFlights it should return correctly flights `() = runBlocking {
-//Given
-            val flightsFlow = getFlight()
-            val expected= getFlight()
+        /*  @Test
+          fun `when flightService#getFlights it should return correctly flights `() = runBlocking {
+  //Given
+              val flightsFlow = getFlight()
+              val expected= getFlight()
 
-            every { flightRepository.findAll() } returns expected
+              every { flightRepository.findAll() } returns expected
 
-            val actual = flightService.getFlights().asFlow()
-            assertEquals(expected, actual)
+              val actual = flightService.getFlights().asFlow()
+              assertEquals(expected, actual)
 
-        }*/
+          }*/
     }
 
     @Nested
@@ -72,6 +76,8 @@ class FlightServiceTest {
         @Test
         fun `when flightService#createFlight it should return correctly Flight `() = runBlocking {
             val createCustomerRequest = createFlightRequest()
+            val flightCreatedEvent = flightCreatedEvent()
+
             val flightToSave = Flight(
                 createCustomerRequest.flightId,
                 createCustomerRequest.pnrCode,
@@ -79,11 +85,15 @@ class FlightServiceTest {
                 createCustomerRequest.departureLocation,
                 createCustomerRequest.phoneNumber
             )
+            coEvery { flightCreatedEventProducer.send(flightCreatedEvent) } just Runs
             coEvery { flightRepository.save(flightToSave) } returns getFlight()
             val expected = getFlight()
             val actual = flightService.createFlight(flightToSave)
             assertEquals(expected, actual)
-            coVerify { flightRepository.save(flightToSave) }
+            coVerify {
+                flightRepository.save(flightToSave)
+                flightCreatedEventProducer.send(flightCreatedEvent)
+            }
         }
 
     }
--- src/test/kotlin/com/example/luggagesystemflightapi/utils/TestUtils.kt
+++ src/test/kotlin/com/example/luggagesystemflightapi/utils/TestUtils.kt
@@ -1,6 +1,7 @@
 package com.example.luggagesystemflightapi.utils
 
 import com.example.luggagesystemflightapi.domain.dto.FlightRequest
+import com.example.luggagesystemflightapi.domain.event.FlightCreatedEvent
 import com.example.luggagesystemflightapi.domain.model.Flight
 import java.time.LocalDate
 import java.util.UUID
@@ -23,4 +24,8 @@ fun createFlightRequest(
     phoneNumber: Int = 12345
 ): FlightRequest {
     return FlightRequest(flightId, pnrCode, flightDate, departureLocation, phoneNumber)
+}
+
+fun flightCreatedEvent(flightId: UUID = UUID.fromString("4aa78416-2f33-42b6-9f17-6d8b9e474174")): FlightCreatedEvent {
+    return FlightCreatedEvent(flightId)
 }
